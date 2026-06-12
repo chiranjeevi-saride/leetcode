b0a0e4ddbac57a9d597da99fb91015da749d5477
chiranjeevi-saride <csaride92@gmail.com>
2026-06-11 23:16:18 -0500

    changes

--- python/643. Maximum Average Subarray I.py
+++ python/643. Maximum Average Subarray I.py
@@ -2,6 +2,7 @@
 Time = O(n)
 Space = O(1)
 '''
+from typing import List
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         max_val = 0
@@ -20,3 +21,14 @@ class Solution:
                 max_val = window_sum
         
         return max_val / k
+    
+
+def main():
+    solution = Solution()
+    #TC1 - 12.75
+    print(solution.findMaxAverage([1,12,-5,-6,50,3], 4))
+    #TC2 - 5.00000
+    print(solution.findMaxAverage([5], 1))
+
+if __name__ == "__main__":    
+    main()
